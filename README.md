28bbf9b99af5359172084b482d3cbe62c7a86b43
dequevedo <danielelvisquevedo@gmail.com>
2021-04-17 19:56:02 -0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node-js-poc
+
+# Instalar o express
+npm install --save express
+
+# Rodar o server
+node server.js
---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res.status(200).send(
+        {
+            mensagem: "Ok, deu certo"
+        }
+    )
+});
+
+module.exports = app
